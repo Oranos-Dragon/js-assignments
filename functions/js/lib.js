a815ce935d41f74bc38eda198ed5b88675f6644a
Oranos-Dragon <302164886@student.rocmondriaan.nl>
2022-11-16 12:58:41 +0100

    update lib
    made it so the numbers dont go in string

--- functions/js/lib.js
+++ functions/js/lib.js
@@ -1,19 +1,19 @@
 function numbers() {
-    const number1 = prompt("choose a number.");
-    const number2 = prompt("choose another number.");
+    const number1 = parseInt(prompt("choose a number."));
+    const number2 = parseInt(prompt("choose another number."));
     const number3 = number1 + number2;
     alert(number1 + "+" + number2 + "=" + number3)
 }
 
 function rectangle() {
-    const height = prompt("What is the height of the rectangle.");
-    const width = prompt("What's the width of the rectangle.");
+    const height = parseInt(prompt("What is the height of the rectangle."));
+    const width = parseInt(prompt("What's the width of the rectangle."));
     const surface = height * width;
     alert("the surface is " + surface + " m2")
 }
 
 function workMemory() {
-    const memory = prompt("How much GB RAM do you have?");
+    const memory = parseInt(prompt("How much GB RAM do you have?"));
     if (memory > 4) {
         alert("You have enough memory for VSC");
     }
@@ -23,7 +23,7 @@ function workMemory() {
 }
 
 function userAge() {
-    const age = prompt("How old are you?")
+    const age = parseInt(prompt("How old are you?"))
     if (age < 18) {
         alert("you did not pass this round")
     }
@@ -33,20 +33,20 @@ function userAge() {
 }
 
 function cricleSurface() {
-    const radius = prompt("What's the radius of the circle");
+    const radius = parseInt(prompt("What's the radius of the circle"));
     const surfaceCircle = 3.14 * (radius * radius);
     alert("The surface of the cirlce is" + surfaceCircle + "m2")
 }
 
 function math() {
-    const no1 = prompt("choose a number");
-    const no2 = prompt("choose another number");
+    const no1 = parseInt(prompt("choose a number"));
+    const no2 = parseInt(prompt("choose another number"));
     const result = ((no1 + no2) * (no1 + no2)) / ((no1 - no2)* (no1 - no2));
     alert(result)
 }
 
 function childrenMoney() {
-    const childrenNumber = prompt("How many children are you signing up to the music school");
+    const childrenNumber = parseInt(prompt("How many children are you signing up to the music school"));
     let money;
     if (childrenNumber == 1) {
         money = 300
@@ -64,7 +64,7 @@ function childrenMoney() {
 }
 
 function cost() {
-    let pagesNumber = prompt("how many pages do you want")
+    let pagesNumber = parseInt(prompt("how many pages do you want"))
     let costs = pagesNumber * 0.05 + 75
     console.log("it costs €" + costs + " without Tax");
     const costsWithTax = costs + (costs * 0.21)
